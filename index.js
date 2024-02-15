1e41c137178f8bea00f99b79df2f23ed726acd69
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-02-14 18:14:43 -0600

    console.log examples

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("Hello World");
+
+let age = null;
+console.log("Age as null:", age);
+``
+age = 20;
+console.log("Age as 20:", age);
+
+const firstName = "John";
+console.log("First Name:", firstName);
+console.log(`My name is ${firstName} and I am ${age} years old.`);
